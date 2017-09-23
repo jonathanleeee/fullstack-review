aa9c40fbe58d095c5079e4fe4c88c5a69e09d61d
Hack Reactor Student <student@hackreactor.com>
2017-09-22 19:51:54 -0700

    worked on schema

--- database/index.js
+++ database/index.js
@@ -3,11 +3,15 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: string,
+  url: string, 
+  description: string
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (/* TODO */ err, ) => {
+  if (err) return console.error(err);
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
